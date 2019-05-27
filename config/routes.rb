3e618fa20c8dceb3dc62c696741e74e5bda8af27
dsound-zz <demiansims@gmail.com>
2019-05-27 15:09:44 -0400

    changed cors

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
-  root to: "api/v1/sightings#index"
+  # root to: "api/v1/sightings#index"
   
   if Rails.env.development?
     scope format: true, constraints: { format: /jpg|png|gif|PNG/ } do
