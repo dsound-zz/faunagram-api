8cfddebb31b4ca0c0e8725e496b0a0aba5d4f850
dsound-zz <demiansims@gmail.com>
2019-05-27 19:16:21 -0400

    changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
-  # root to: "api/v1/sightings#index"
+  root to: "api/v1/sightings#index"
   
   if Rails.env.development?
     scope format: true, constraints: { format: /jpg|png|gif|PNG/ } do
-      get '/*anything', to: proc { [404, {}, ['']] }, constraints: lambda { |request| !request.path_parameters[:anything].start_with?('rails/') }
+      get '*anything', to: proc { [404, {}, ['']] }, constraints: lambda { |request| !request.path_parameters[:anything].start_with?('rails/') }
     end
   end
 
@@ -18,14 +18,14 @@ Rails.application.routes.draw do
       resources :users, only: [:index, :show, :create, :update, :destroy] do
         resources :comments
       end
-        get "/current_user", to: "auth#get_user_from_token"
-        post "/login", to: "auth#login"
-        put "/users/:id/avatar_upload/",   to: "users#avatar_upload"
+        get "current_user", to: "auth#get_user_from_token"
+        post "login", to: "auth#login"
+        put "users/:id/avatar_upload/",   to: "users#avatar_upload"
       resources :animals, only: [:index, :show, :update]
       resources :sightings, only: [:index, :show, :create, :update, :destroy] do
         resources :comments
       end
-        put "/sightings/:id/image_upload/", to: "sightings#image_upload_update"
+        put "sightings/:id/image_upload/", to: "sightings#image_upload_update"
 
       resources :comments, only: [:index, :create, :update, :destroy] do
         resources :comments
